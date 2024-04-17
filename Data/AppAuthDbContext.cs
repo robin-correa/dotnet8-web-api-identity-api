044570752cf6eb4e08e076e4cd94debfc7479400
Robin Jimmy Lagrama Correa <robincorrea@axs.sg>
2024-04-17 15:01:20 +0800

    Implement authentication with default Identity package in .NET 8.

---
+++ Data/AppAuthDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet8_web_api_identity_api.Data
+{
+    public class AppAuthDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppAuthDbContext(DbContextOptions<AppAuthDbContext> options) : base(options)
+        { }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
+using dotnet8_web_api_identity_api.Data;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+using Swashbuckle.AspNetCore.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,9 +13,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Enter 'Bearer' followed by a space and the accessToken",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+builder.Services.AddDbContext<AppAuthDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppAuthDbConnectionString"));
+});
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<AppAuthDbContext>();
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
+
+app.MapGroup("/api/auth").MapIdentityApi<IdentityUser>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
